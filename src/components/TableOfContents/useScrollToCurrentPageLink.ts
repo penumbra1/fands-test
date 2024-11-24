b6c872b125620128deecf12542c3486e0c5c08ca
Olga Isakova <penumbral@yandex.ru>
2024-11-24 20:17:21 +0300

    prevent 'querySelector is not a function' error

--- src/components/TableOfContents/useScrollToCurrentPageLink.ts
+++ src/components/TableOfContents/useScrollToCurrentPageLink.ts
@@ -2,7 +2,8 @@ import { watchEffect, type ComponentPublicInstance, type ShallowRef } from 'vue'
 
 export function useScrollToCurrentPageLink(list: ShallowRef<ComponentPublicInstance | null>): void {
   const unwatch = watchEffect(() => {
-    const currentPageLink = list?.value?.$el.querySelector('[aria-current="page"]')
+    const currentPageLink = list?.value?.$el?.querySelector?.('[aria-current="page"]')
+
     if (currentPageLink) {
       currentPageLink?.scrollIntoView()
       unwatch()
